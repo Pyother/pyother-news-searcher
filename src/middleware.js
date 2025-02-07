a0d1d161a5ac04487546b769e9816ed5eb6e1c92
Pyother <piotr.dominik.sobol@gmail.com>
2025-02-07 22:29:58 +0100

    Apply middleware for testing

---
+++ src/middleware.js
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+    // console.log('Newsdata API key: ' + process.env.NEWSDATA_API_KEY);
+}
+ 
