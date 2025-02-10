b4a910e2839a518733f1a2cf7d34f1a2df458ee0
Pyother <piotr.dominik.sobol@gmail.com>
2025-02-10 19:05:50 +0100

    Debugging for AWS Amplify

--- src/app/api/newsdata/route.js
+++ src/app/api/newsdata/route.js
@@ -1,13 +1,12 @@
 export async function GET(request) {
-    
+    console.log("API Key:", process.env.NEWSDATA_API_KEY); // Debugowanie
+
     const response = await fetch(
-        'https://newsdata.io/api/1/news?apikey=' + process.env.NEWSDATA_API_KEY + '=inter%20mediolan'
+        `https://newsdata.io/api/1/news?apikey=${process.env.NEWSDATA_API_KEY}&q=inter%20mediolan`
     );
 
     const data = await response.json();
     return new Response(JSON.stringify(data), {
-        headers: {
-            'content-type': 'application/json',
-        },
+        headers: { 'content-type': 'application/json' },
     });
 }
