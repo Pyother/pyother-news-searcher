1e29ca9f7e2161096c2cbde434ef1d65f88d4c98
pyother <piotr.dominik.sobol@gmail.com>
2025-06-20 14:14:26 +0200

    Launch Poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
 import { Metadata } from 'next';
+import { Poppins } from 'next/font/google'
+import './globals.css';
+
+const poppins = Poppins({
+    subsets: ['latin'],
+    weight: ['100', '300']
+})
 
 export const Metadata = {
     title: 'pyother-news-searcher'
@@ -11,7 +18,7 @@ export default function RootLayout({
 }>) {
         
     return (
-        <html lang="en">
+        <html lang="en" className={poppins.className}>
             <body>
                 {children}
             </body>
